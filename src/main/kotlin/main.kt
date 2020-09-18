b8970643cc5435a548b964d142f7bf026661ba47
Anastasiya Rassolova <avmurueva@gmail.com>
2020-09-18 10:44:57 +0300

    initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,39 @@
+fun main() {
+    val price = 100
+    val count = 10
+
+    val regularCustomer = true
+    val lastPurchase = 11_000 //сумма ПРЕДЫДУЩЕЙ покупки, по условиям задачи от нее зависят скидки
+
+    val discountStart = 1_000
+    val discount = 100
+
+    val discountStartBigPurchase = 10_000
+    val discountBigPurchase = 0.05
+
+    val discountForRegularCustomer = 0.01
+
+    val currentPurchase = price * count
+
+    val currentPurchaseResult =
+        if (regularCustomer) {
+            if (lastPurchase <= discountStart) currentPurchase - currentPurchase * discountForRegularCustomer
+            else if (lastPurchase > discountStart && lastPurchase <= discountStartBigPurchase) {
+                (currentPurchase - discount) - (currentPurchase - discount) * discountForRegularCustomer
+            } else {
+                (currentPurchase - currentPurchase * discountBigPurchase) - (currentPurchase - currentPurchase * discountBigPurchase) * discountForRegularCustomer
+            }
+        } else {
+            if (lastPurchase <= discountStart) {
+                currentPurchase
+            } else if (lastPurchase > discountStart && lastPurchase <= discountStartBigPurchase) {
+                currentPurchase - discount
+            } else {
+                currentPurchase - currentPurchase * discountBigPurchase
+            }
+        }
+
+    println("Сумма покупки: $currentPurchase"
+            +"\nСумма покупки со скидками: $currentPurchaseResult"
+    )
+}
